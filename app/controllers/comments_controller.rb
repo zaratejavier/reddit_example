4a7df12d9e08949f78dc6b94a98ad0b73a79748d
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-20 14:31:58 -0600

    done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,19 @@ before_action :set_topic
     @comment = Comment.new
   end
 
+  def create
+    @comment = @topic.comments.new(comment_params)
+    if @comment.save
+      redirect_to sub_topic_path(@topic.sub_id, @topic)
+    else
+      render :new
+    end
+  end
+
   private
+  def comment_params
+    params.require(:comment).permit(:author, :body)
+  end
 
   def set_topic
     @topic = Topic.find(params[:topic_id])
