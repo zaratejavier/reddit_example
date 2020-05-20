292f6f929f08acd98c38dfc3576bdb038bcf5fcb
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-20 14:33:10 -0600

    check if author exist

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ApplicationRecord
+  before_save :check_author
+
   belongs_to :topic
+
+  def check_author
+    if self.author.blank?
+      self.author = 'anon'
+      
+  end
+end
 end
